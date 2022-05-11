7f31715b54c2b0a2c1b0b13f94ccda72b25d7d3d
TajHarker <tharker@student.saintaug.nsw.edu.au>
2022-05-12 09:14:03 +1000

    Password generator, backend
    Beginning the password encoding, backend

---
+++ Encode_password/encode_algorithm.py
@@ -0,0 +1,27 @@
+import random
+
+#String
+string = "abcdefghijklmnopqrstuvwxyz0123456789!@#$%*()"
+array = []
+for c in string:
+  array += [c]
+ 
+for i in range(8):
+  randpassword = []
+  ((array[random.randint(0, len(array)-1)]))
+
+
+  
+
+# Faster and more efficient
+#random.choice(string)
+
+
+
+
+  
+
+
+
+
+    
