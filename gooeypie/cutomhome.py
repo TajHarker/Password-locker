dc13ba9dccd51498bb7f03e6a08ade03cf5be733
TajHarker <tharker@student.saintaug.nsw.edu.au>
2022-06-14 14:24:15 +1000

    Switching between windows
    Finally worked out how to use deiconify() and withdraw() in tkinter.

--- gooeypie/cutomhome.py
+++ gooeypie/cutomhome.py
@@ -7,30 +7,34 @@ customtkinter.set_appearance_mode("dark")  # Modes: "System" (standard), "Dark",
 customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
 app = customtkinter.CTk()
 app.geometry("600x600")
-app.title("CustomTkinter simple_example.py")
+app.title("Password-Locker")
 
 def close():
     app.deiconify
 
-def signup():
-    app = customtkinter.CTk()
-    app.geometry("600x600")
-    app.title("CustomTkinter simple_example.py")
+def signup_page():
+    signup = customtkinter.CTkToplevel(master=app)
+    signup.geometry("600x600")
+    signup.title("Password-Locker")
 
-    frame_1 = customtkinter.CTkFrame(master=app)
+    def passgen_button():
+        app.withdraw()
+        passgen_page()
+
+    frame_1 = customtkinter.CTkFrame(master=signup)
     frame_1.pack(pady=60, padx=60, fill="both", expand=True)
 
-    button_1 = customtkinter.CTkButton(master=frame_1, command=passwordgen, text="Generate Primary Key")
+    button_1 = customtkinter.CTkButton(master=frame_1, command=passgen_button, text="Generate Primary Key")
     button_1.place(relx=0.5, rely=0.5, anchor=CENTER)
 
     app.mainloop()
 
-def passwordgen():
-    app = customtkinter.CTk()
-    app.geometry("600x600")
-    app.title("CustomTkinter simple_example.py")
+def passgen_page():
+    passgen = customtkinter.CTkToplevel(master=app)
+    passgen.geometry("600x600")
+    passgen.title("Password-Locker")
     
-    frame_1 = customtkinter.CTkFrame(master=app)
+    frame_1 = customtkinter.CTkFrame(passgen=app)
     frame_1.pack(pady=60, padx=60, fill="both", expand=True)
 
     i = 0
@@ -49,32 +53,30 @@ def passwordgen():
     label_1.pack(pady=12, padx=10)
     
 def main():
-    app = customtkinter.CTk()
-    app.geometry("600x600")
-    app.title("CustomTkinter simple_example.py")
-
     frame_1 = customtkinter.CTkFrame(master=app)
     frame_1.pack(pady=60, padx=60, fill="both", expand=True)
 
-    button_1 = customtkinter.CTkButton(master=frame_1, command=login_page, text="Log In")
+    def login_button():
+        app.withdraw()
+        login_page()
+
+    def signup_button():
+        app.withdraw()
+        signup_page()
+        
+    button_1 = customtkinter.CTkButton(master=frame_1, command=login_button, text="Log In")
     button_1.place(relx=0.5, rely=0.4, anchor=CENTER)
 
-    button_1 = customtkinter.CTkButton(master=frame_1, command=signup, text="Sign Up")
+    button_1 = customtkinter.CTkButton(master=frame_1, command=signup_button, text="Sign Up")
     button_1.place(relx=0.5, rely=0.6, anchor=CENTER)
 
-    app.deiconify()
     app.mainloop()
 
 def login_page():
-    customtkinter.set_appearance_mode("dark")  # Modes: "System" (standard), "Dark", "Light"
-    customtkinter.set_default_color_theme("blue")  # Themes: "blue" (standard), "green", "dark-blue"
-
-    textWidget.tag_config('hidden', elide=True)
-
-    app = customtkinter.CTk()
-    app.geometry("600x600")
-    app.title("CustomTkinter simple_example.py")
-    frame_1 = customtkinter.CTkFrame(master=app)
+    login = customtkinter.CTkToplevel(master=app)
+    login.geometry("600x600")
+    login.title("Password-Locker")
+    frame_1 = customtkinter.CTkFrame(master=login)
     frame_1.pack(pady=60, padx=60, fill="both", expand=True)
 
     entry_1 = customtkinter.CTkEntry(master=frame_1, placeholder_text="")
@@ -94,7 +96,7 @@ def login_page():
 def encdec():
     app = customtkinter.CTk()
     app.geometry("600x600")
-    app.title("CustomTkinter simple_example.py")
+    app.title("Password-Locker")
     frame_1 = customtkinter.CTkFrame(master=app)
     frame_1.pack(pady=60, padx=60, fill="both", expand=True)
 
