2a300b3a94d307642d893f3878855040c08c4345
TajHarker <tharker@student.saintaug.nsw.edu.au>
2022-06-16 18:55:57 +1000

    Making pages link and connecting to backend
    Now you can generate the primary key via the 'Generate Password' button. Along with being able to access the encode and decode page

--- gooeypie/cutomhome.py
+++ gooeypie/cutomhome.py
@@ -18,7 +18,7 @@ def signup_page():
     signup.title("Password-Locker")
 
     def passgen_button():
-        app.withdraw()
+        signup.withdraw()
         passgen_page()
 
     frame_1 = customtkinter.CTkFrame(master=signup)
@@ -33,24 +33,45 @@ def passgen_page():
     passgen = customtkinter.CTkToplevel(master=app)
     passgen.geometry("600x600")
     passgen.title("Password-Locker")
-    
-    frame_1 = customtkinter.CTkFrame(passgen=app)
-    frame_1.pack(pady=60, padx=60, fill="both", expand=True)
+
+    def login_button():
+        passgen.withdraw()
+        login_page()
 
     i = 0
     array = []
     while i < 3:
-        lines = open("words.txt").readlines() 
+        lines = open("gooeypie/words.txt").readlines() 
         myword = random.choice(lines)
         myword = str(myword)
         array.append(myword)
         i += 1
 
-    array = (array[0] + array[1] + array[2])
-    label_1 = customtkinter.CTkLabel(master=frame_1, justify=tkinter.LEFT)
+    frame_1 = customtkinter.CTkFrame(master=passgen)
+    frame_1.pack(pady=20, padx=60, fill="both", expand=True)
 
-    #label_1 = customtkinter.CTkLabel(master=frame_1, justify=tkinter.LEFT, text="Your Primary Keys Are:" + (array))
+    label_1 = customtkinter.CTkLabel(master=frame_1, text=array[0], justify=tkinter.LEFT)
     label_1.pack(pady=12, padx=10)
+    label_1.place(relx=0.2, rely=0.55, anchor=CENTER)
+    label_1. configure(font=("Futura", 20))
+
+    label_2 = customtkinter.CTkLabel(master=frame_1, text=array[1], justify=tkinter.LEFT)
+    label_2.pack(pady=12, padx=10)
+    label_2.place(relx=0.5, rely=0.55, anchor=CENTER)
+    label_2. configure(font=("Futura", 20))
+
+    label_3 = customtkinter.CTkLabel(master=frame_1, text=array[2], justify=tkinter.LEFT)
+    label_3.pack(pady=12, padx=10)
+    label_3.place(relx=0.8, rely=0.55, anchor=CENTER)
+    label_3. configure(font=("Futura", 20)) 
+
+    label_4 = customtkinter.CTkLabel(master=frame_1, text='Your Primary Keys Are:', justify=tkinter.LEFT)
+    label_4.pack(pady=12, padx=10)
+    label_4.place(relx=0.5, rely=0.3, anchor=CENTER)
+    label_4. configure(font=("Futura", 30))
+
+    button_1 = customtkinter.CTkButton(master=frame_1, command=login_button, text="Login")
+    button_1.place(relx=0.5, rely=0.8, anchor=CENTER)
     
 def main():
     frame_1 = customtkinter.CTkFrame(master=app)
@@ -67,8 +88,8 @@ def main():
     button_1 = customtkinter.CTkButton(master=frame_1, command=login_button, text="Log In")
     button_1.place(relx=0.5, rely=0.4, anchor=CENTER)
 
-    button_1 = customtkinter.CTkButton(master=frame_1, command=signup_button, text="Sign Up")
-    button_1.place(relx=0.5, rely=0.6, anchor=CENTER)
+    button_2 = customtkinter.CTkButton(master=frame_1, command=signup_button, text="Sign Up")
+    button_2.place(relx=0.5, rely=0.6, anchor=CENTER)
 
     app.mainloop()
 
@@ -79,31 +100,78 @@ def login_page():
     frame_1 = customtkinter.CTkFrame(master=login)
     frame_1.pack(pady=60, padx=60, fill="both", expand=True)
 
+    def login_button():
+        login.withdraw()
+        encdec_page()
+
+    label_1 = customtkinter.CTkLabel(master=frame_1, text='Login', justify=tkinter.LEFT)
+    label_1.pack(pady=12, padx=10)
+    label_1.place(relx=0.5, rely=0.2, anchor=CENTER)
+    label_1. configure(font=("Helveta", 35,))
+
     entry_1 = customtkinter.CTkEntry(master=frame_1, placeholder_text="")
-    entry_1.place(relx=0.5, rely=0.2, anchor=CENTER)
+    entry_1.place(relx=0.5, rely=0.4, anchor=CENTER)
 
     entry_2 = customtkinter.CTkEntry(master=frame_1, placeholder_text="")
-    entry_2.place(relx=0.5, rely=0.3, anchor=CENTER)
+    entry_2.place(relx=0.5, rely=0.5, anchor=CENTER)
 
     entry_3 = customtkinter.CTkEntry(master=frame_1, placeholder_text="")
-    entry_3.place(relx=0.5, rely=0.4, anchor=CENTER)
+    entry_3.place(relx=0.5, rely=0.6, anchor=CENTER)
 
-    button_1 = customtkinter.CTkButton(master=frame_1, command=encdec, text="Log In")
+    button_1 = customtkinter.CTkButton(master=frame_1, command=login_button, text="Log In")
     button_1.place(relx=0.5, rely=0.8, anchor=CENTER)
 
     app.mainloop()
 
-def encdec():
-    app = customtkinter.CTk()
-    app.geometry("600x600")
-    app.title("Password-Locker")
-    frame_1 = customtkinter.CTkFrame(master=app)
+def encdec_page():
+    encdec = customtkinter.CTk()
+    encdec.geometry("600x600")
+    encdec.title("Password-Locker")
+    frame_1 = customtkinter.CTkFrame(master=encdec)
     frame_1.pack(pady=60, padx=60, fill="both", expand=True)
 
-    button_1 = customtkinter.CTkButton(master=frame_1, command=login_page, text="Log In")
+    def encode_button():
+        encdec.withdraw()
+        encode_page()
+
+    def decode_button():
+        encdec.withdraw()
+        decode_page()
+
+    button_1 = customtkinter.CTkButton(master=frame_1, command=encode_button, text="Encode")
     button_1.place(relx=0.5, rely=0.4, anchor=CENTER)
 
-    button_1 = customtkinter.CTkButton(master=frame_1, command=close, text="Sign Up")
+    button_2 = customtkinter.CTkButton(master=frame_1, command=decode_button, text="Decode")
+    button_2.place(relx=0.5, rely=0.6, anchor=CENTER)
+
+    app.mainloop()
+
+def encode_page():
+    enc = customtkinter.CTk()
+    enc.geometry("600x600")
+    enc.title("Password-Locker")
+    frame_1 = customtkinter.CTkFrame(master=enc)
+    frame_1.pack(pady=60, padx=60, fill="both", expand=True)
+
+    entry_1 = customtkinter.CTkEntry(master=frame_1, placeholder_text="")
+    entry_1.place(relx=0.5, rely=0.45, anchor=CENTER)
+
+    button_1 = customtkinter.CTkButton(master=frame_1, command=signup_page, text="Encode")
+    button_1.place(relx=0.5, rely=0.6, anchor=CENTER)
+
+    app.mainloop()
+
+def decode_page():
+    enc = customtkinter.CTk()
+    enc.geometry("600x600")
+    enc.title("Password-Locker")
+    frame_1 = customtkinter.CTkFrame(master=enc)
+    frame_1.pack(pady=60, padx=60, fill="both", expand=True)
+
+    entry_1 = customtkinter.CTkEntry(master=frame_1, placeholder_text="")
+    entry_1.place(relx=0.5, rely=0.45, anchor=CENTER)
+
+    button_1 = customtkinter.CTkButton(master=frame_1, command=signup_page, text="Decode")
     button_1.place(relx=0.5, rely=0.6, anchor=CENTER)
 
     app.mainloop()
